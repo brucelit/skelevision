5c493425d13edb5c6c7734732c64a9eecfd26b72
brucelit <brucelit@163.com>
2019-11-10 20:20:23 +0100

    'always_relationship'

--- skelevision/exceptions.py
+++
@@ -1,5 +0,0 @@
-class IllegalLogAction(Exception):
-    """Raised when an incorrect action is performed on a log (e.g.
-    in a trace-to-frequency log import duplicated traces)
-    """
-    pass
--- tests/test_miners.py
+++
@@ -1,9 +0,0 @@
-import os
-
-HERE = os.path.dirname(os.path.abspath(__file__))
-DATA = os.path.join(HERE, "datasets")
-
-
-class TestLogSkeleton(object):
-
-    pass
